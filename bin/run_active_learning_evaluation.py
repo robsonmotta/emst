66af34cb025d4ad3f79026ce568882fde33b5647
Robson Motta <robsonmotta@gmail.com>
2015-07-15 01:02:36 -0300

    first classifier structure

--- bin/run_active_learning_evaluation.py
+++ bin/run_active_learning_evaluation.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python
 import _mypath
 from preprocessing import splitted_data_from_csv
+from active_learning import classify_folds
 
 if __name__ == '__main__':
     dataset, dataset_folds, distance_matrix = splitted_data_from_csv('/Users/robsonmotta/Documents/git/emst/fixtures/iris.csv')
     dataset_X, dataset_y = dataset
     dataset_X_train_10fold, dataset_X_test_10fold, dataset_y_train_10fold, dataset_y_test_10fold = dataset_folds
+
+    accuracy_mean = classify_folds(dataset_X_train_10fold, dataset_X_test_10fold, dataset_y_train_10fold, dataset_y_test_10fold)
     
-    print distance_matrix
+    print "accuracy_mean", accuracy_mean
--- lib/active_learning/active_learning.py
+++ lib/active_learning/active_learning.py
@@ -1,4 +1,18 @@
 #! /usr/bin/env python
-def squares2(start, stop):
-    for i in range(0, 10):
-        print i**2
+
+import numpy as np
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+
+def classify_folds(X_train_10fold, X_test_10fold, y_train_10fold, y_test_10fold, n_fold=10, n_neighbors=5):
+    accuracies = []
+    for fold in range(n_fold):
+        X_train = X_train_10fold[fold]
+        X_test = X_test_10fold[fold]
+        y_train = y_train_10fold[fold]
+        y_test = y_test_10fold[fold]
+        classifier = KNeighborsClassifier(n_neighbors)
+        classifier.fit(X_train, y_train)
+        y_pred = classifier.predict(X_test)
+        accuracies.append(metrics.accuracy_score(y_test, y_pred))
+    return np.mean(accuracies)
